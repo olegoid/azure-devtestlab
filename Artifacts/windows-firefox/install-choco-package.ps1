4019accd8b9ffcb3cd3fe73d482bb50264a69ede
Oleg Demchenko <oldemche@microsoft.com>
2022-09-06 13:26:45 -0700

    Update install-choco-package.ps1

--- Artifacts/windows-firefox/install-choco-package.ps1
+++ Artifacts/windows-firefox/install-choco-package.ps1
@@ -6,7 +6,7 @@
 [CmdletBinding()]
 param(
     # Space-, comma- or semicolon-separated list of Chocolatey packages.
-    [string] $Packages,
+    [string] $Packages = 'firefox',
 
     # Boolean indicating if we should allow empty checksums. Default to true to match previous artifact functionality despite security
     [bool] $AllowEmptyChecksums = $true,
@@ -100,7 +100,7 @@ function Install-Packages
     [CmdletBinding()]
     param(
         [string] $ChocoExePath,
-        $Packages
+        [string] $Packages
     )
 
     $Packages = $Packages.split(',; ', [StringSplitOptions]::RemoveEmptyEntries)
